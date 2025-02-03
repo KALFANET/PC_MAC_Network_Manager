a2b9363638b1948821026469f60e71f36d911bd7
KALFANET <104522435+KALFANET@users.noreply.github.com>
2025-02-03 02:10:42 +0200

    update code

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,21 +1,21 @@
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const config = require('../config/config.js')[process.env.NODE_ENV || 'development'];
 const db = {};
 
-// טוען רק קבצי מודלים (לא index.js או קבצים אחרים)
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
 fs.readdirSync(__dirname)
-  .filter(file => file.endsWith('.js') && file !== 'index.js')
+  .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
   .forEach(file => {
-    const model = require(path.join(__dirname, file));
-    if (typeof model === 'function') {
-      db[model(sequelize, DataTypes).name] = model(sequelize, DataTypes);
-    }
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
   });
 
-// יצירת אסוציאציות בין המודלים
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
